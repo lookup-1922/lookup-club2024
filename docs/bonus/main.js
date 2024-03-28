92d2509bc6dd26dff51dbb4c881b3c0a107b399d
lookup <125456965+lookup-1922@users.noreply.github.com>
2024-03-28 10:32:02 +0900

    format

--- docs/bonus/main.js
+++ docs/bonus/main.js
@@ -4,13 +4,13 @@ phina.globalize();
 // MainScene クラスを定義
 phina.define('MainScene', {
   superClass: 'DisplayScene',
-  init: function() {
+  init: function () {
     this.superInit();
     // 背景色を指定
     this.backgroundColor = '#222';
 
     // 星を表示
-    for (let i=0; i<32; ++i) {
+    for (let i = 0; i < 32; ++i) {
       let star = StarShape().addChildTo(this);
       star.x = Math.randint(0, this.width);
       star.y = Math.randint(0, this.height);
@@ -18,15 +18,15 @@ phina.define('MainScene', {
 
       star.stroke = 'transparent';
 
-      star.update = function() {
-        this.rotation+=4;
+      star.update = function () {
+        this.rotation += 4;
       };
     }
   },
 });
 
 // メイン処理
-phina.main(function() {
+phina.main(function () {
   // アプリケーション生成
   var app = GameApp({
     startLabel: 'main', // メインシーンから開始する
